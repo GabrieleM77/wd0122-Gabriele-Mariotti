d3153cbe42d3d70f6ff063214862fc9a03fe13db
GabrieleM77 <gabrimariotti7@gmail.com>
2022-05-13 14:18:19 +0200

    esercizio m2 w3 d5

---
+++ m2/w3/d5/progetto giorno 5/script/script.js
@@ -0,0 +1,35 @@
+fetch("json/users.json")
+.then(res => res.json())
+.then(res => {
+    console.log(res);
+    
+    for(let utente of res){
+
+
+        let tr = document.createElement('tr')
+
+
+        let tdUsername = document.createElement('td')
+        tdUsername.innerHTML = utente.username
+        
+        let tdNome = document.createElement('td')
+        tdNome.innerHTML = utente.firstName
+
+        let tdCognome = document.createElement('td')
+        tdCognome.innerHTML = utente.lastName
+
+        let tdGender = document.createElement('td')
+        tdGender.innerHTML = utente.gender
+
+        let tdEmail = document.createElement('td')
+        tdEmail.innerHTML = utente.email
+
+        let tdPhoto1 = document.createElement('td')
+        tdPhoto1.innerHTML = `<img src="${utente.profileURL}" width=150px>`
+
+        tr.append(tdUsername, tdNome, tdCognome, tdGender, tdEmail, tdPhoto1)
+
+        let tbody = document.querySelector('#utenti')
+        tbody.append(tr)
+    }
+})
